af3b0464ecb7d040eb6f42dfbf0a3526b47672ee
Lindsay Wardell <three060@gmail.com>
2020-12-22 08:21:40 -0800

    chore: configuration cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     'prettier/vue',
     'plugin:prettier/recommended',
     'plugin:nuxt/recommended',
+    'eslint-config-prettier',
   ],
   plugins: ['prettier'],
   // add your custom rules here
